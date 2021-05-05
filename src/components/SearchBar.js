eb281c9a83fa48cedfbd03ecda2b4f1dbf05b561
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-05 11:39:54 -0700

    working error toast

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import API from '../utils/API'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import API from '../utils/API';
+import store from '../config/store';
+
+const Alert = (props) => {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,12 +20,13 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const SearchBar = () => {
+const SearchBar = (props) => {
     const classes = useStyles();
 
     const [searchState, setSearchState] = useState({
         search: ''
     });
+    const [openState, setOpenState] = useState(false)
 
     const handleInputChange = e => {
         const { name, value } = e.target;
@@ -28,15 +36,38 @@ const SearchBar = () => {
         });
     }
 
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenState(false);
+    };
+
     const handleSubmit = e => {
         e.preventDefault();
         API.searchByTitle(searchState.search).then(res => {
-            console.log(res)
+            if (res.data.Response === 'False') {
+                setOpenState(true)
+            } else {
+                store.dispatch({
+                    type: 'SEARCH_MOVIES',
+                    payload: {
+                        results: res.data.Search
+                    }
+                })
+            }
+
         });
     }
 
     return (
         <form className={classes.root} noValidate autoComplete="off">
+            <Snackbar open={openState} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error">
+                    Oops! The Galactic Council has denied your request. Please try again later.
+                </Alert>
+            </Snackbar>
             <TextField id="outlined-basic" label="Search" variant="outlined" name="search" onChange={handleInputChange} />
             <br />
             <Button variant="contained" onClick={handleSubmit} >Submit</Button>
